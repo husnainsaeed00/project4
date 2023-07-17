74f10bcc46aa7b896197fa80eb86d492c6ebb831
Husnain Saeed <husnainsaeed000@gmail.com>
2023-07-17 06:00:17 -0700

    2nd day

--- network/views.py
+++ network/views.py
@@ -4,18 +4,51 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.core.paginator import Paginator
-from .models import User,Post,Follow
+from .models import User,Post,Follow,Like
+import json
+from django.http import JsonResponse
 
+def edit(request, post_id):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        edit_post = Post.objects.get(pk=post_id)
+        edit_post.content =data['content']
+        edit_post.save()
+        return JsonResponse({"message":"Like added"})
 
+def remove_like(request,post_id):
+    post = Post.objects.get(pk=post_id)
+    user = User.objects.get(pk=request.user.id)
+    like = Like.objects.filter(user=user,post=post)
+    like.delete()
+    return JsonResponse({"message":"Disliked"})
+
+def add_like(request,post_id):
+    post = Post.objects.get(pk=post_id)
+    user = User.objects.get(pk=request.user.id)
+    newLike = Like(user=user, post=post)
+    newLike.save()
+    return JsonResponse({"message":"Like Added"})
 def index(request):
     allPosts= Post.objects.all().order_by("id").reverse()
-    paginator = Paginator(allPosts,1)
+    paginator = Paginator(allPosts,10)
     page_number= request.GET.get('page')
     posts_of_the_page = paginator.get_page(page_number)
+    allLikes = Like.objects.all()
+    whoYouLiked = []
+    try:
+        for like in allLikes:
+            if like.user.id == request.user.id:
+                whoYouLiked.append(like.post.id)
+    except:
+        whoYouLiked=[]
     return render(request,"network/index.html",{
         "allPosts":allPosts,
-        "posts_of_the_page":posts_of_the_page
+        "posts_of_the_page":posts_of_the_page,
+        "whoYouLiked":whoYouLiked
     })
+
+
 
 
 def newPost(request):
@@ -31,45 +64,68 @@ def profile(request, user_id):
     user = User.objects.get(pk=user_id)
     allPosts = Post.objects.filter(user=user).order_by("id").reverse()
 
-    following=Follow.objects.filter(user=user)
-    followers=Follow.objects.filter(user_follower=user)
-    try:
-        checkFollow= followers.filter(user=user.objects.get(pk=request.user.id))
-        if len(checkFollow) !=0:
-            isFollowing=True
-        else:
-            isFollowing=False
-    except:
-        isFollowing=False
-    #pagination
+    following = Follow.objects.filter(user=user)
+    followers = Follow.objects.filter(user_follower=user)
+    isFollowing = False
+
+    if request.user.is_authenticated:
+        checkFollow = followers.filter(user=request.user)
+        if checkFollow.exists():
+            isFollowing = True
+
+    # Pagination
     paginator = Paginator(allPosts, 1)
     page_number = request.GET.get('page')
     posts_of_the_page = paginator.get_page(page_number)
+
     return render(request, "network/profile.html", {
         "posts_of_the_page": posts_of_the_page,
         "username": user.username,
-        "following":following,
-        "followers":followers,
-        "isFollowing":isFollowing,
-        "user_profile":user
-
+        "following": following,
+        "followers": followers,
+        "isFollowing": isFollowing,
+        "user_profile": user
     })
+
+def following (request):
+    currentUser = User.objects.get(pk=request.user.id)
+    followingPeople= Follow.objects.filter(user=currentUser)
+    allPosts= Post.objects.all().order_by('id').reverse()
+
+    followingPosts = []
+
+    for post in allPosts:
+        for person in followingPeople:
+            if person.user_follower == post.user:
+                followingPosts.append(post)
+    paginator = Paginator(followingPosts,10)
+    page_number= request.GET.get('page')
+    posts_of_the_page = paginator.get_page(page_number)
+    return render(request,"network/following.html",{
+        "posts_of_the_page":posts_of_the_page
+    })
+
 def follow(request):
     userfollow = request.POST['userfollow']
-    currentUser =User.objects.get(pk=request.user.id)
-    userfollowData=User.objects.get(username=userfollow)
-    f = Follow(user=currentUser,user_follower=userfollowData)
+    currentUser = User.objects.get(pk=request.user.id)
+    userfollowData = User.objects.get(username=userfollow)
+    f = Follow(user=currentUser, user_follower=userfollowData)
     f.save()
-    user_id=userfollowData.id
+    user_id = userfollowData.id
     return HttpResponseRedirect(reverse('profile', kwargs={'user_id': user_id}))
+
 def unfollow(request):
     userfollow = request.POST['userfollow']
-    currentUser =User.objects.get(pk=request.user.id)
-    userfollowData=User.objects.get(username=userfollow)
-    f = Follow.objects.get(user=currentUser,user_follower=userfollowData)
-    f.delete()
-    user_id=userfollowData.id
+    currentUser = User.objects.get(pk=request.user.id)
+    userfollowData = User.objects.get(username=userfollow)
+    follows = Follow.objects.filter(user=currentUser, user_follower=userfollowData)
+    
+    # Delete all the follow relationships
+    follows.delete()
+    
+    user_id = userfollowData.id
     return HttpResponseRedirect(reverse('profile', kwargs={'user_id': user_id}))
+
 def login_view(request):
     if request.method == "POST":
 
